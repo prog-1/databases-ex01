b2e8a182c5195570edec99a0c83a59a54383dd2a
Sp0ozy <imolchanov210@gmail.com>
2022-04-13 22:05:07 +0300

    improved

--- database/Exams.go
+++ database/Exams.go
@@ -10,6 +10,22 @@ type ExamsTable struct {
 	Data []Exams
 }
 
+func (db *ExamsTable) Insert(sID, lID, grade int) Exams {
+	ex := Exams{
+		ID:        len(db.Data) + 1,
+		StudentID: sID,
+		LessonID:  lID,
+		Grade:     grade,
+	}
+	db.Data = append(db.Data, ex)
+	return ex
+}
+
+func (db *ExamsTable) AddExam(sID int, lID int, grade ...int) {
+	for _, le := range grade {
+		db.Insert(sID, lID, le)
+	}
+}
 func (db *ExamsTable) ExamFindById(id int) Exams {
 	for _, c := range db.Data {
 		if c.ID == id {
