5b6c240d9d08ebeeafdd354e7d05129878733e46
vmaksimkina <valerijamax@gmail.com>
2022-04-16 20:35:30 +0300

    done

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 )
 
 type tables struct {
+	exams     database.ExamsTable
 	students  database.StudentTable
 	classes   database.ClassTable
 	groups    database.GroupTable
@@ -15,13 +16,12 @@ type tables struct {
 }
 
 func studentCountPerClass(db tables) map[string]int {
-	//it counts classes too . . .
+	//improved
 	m := make(map[string]int)
 	for _, i := range db.groups.Data {
 		for _, j := range db.classes.Data {
-			class := db.classes.MustFind(j.Year, j.Mod)
 			if i.ClassID == j.ID {
-				m[fmt.Sprint(class)]++
+				m[fmt.Sprint(j.Year)+j.Mod]++
 			}
 		}
 	}
@@ -40,8 +40,64 @@ func studentCountPerYear(db tables) map[int]int {
 	return m
 }
 
+func lessonsPerYear(db tables, year int) (str []string) {
+	for _, timetable := range db.timetable.Data {
+		for _, class := range db.classes.Data {
+			for _, lesson := range db.lessons.Data {
+				if timetable.ClassID == class.ID && class.Year == year && lesson.ID == timetable.LessonID {
+					str = append(str, lesson.Name)
+				}
+			}
+		}
+	}
+	return unique(str)
+}
+
+func unique(s []string) []string {
+	m := make(map[string]int)
+	var result []string
+	for _, str := range s {
+		if _, ok := m[str]; !ok {
+			m[str] = 1
+			result = append(result, str)
+		}
+	}
+	return result
+}
+
+func examsPerClass(db tables, year int, mod string) (str []string) {
+	for _, timetable := range db.timetable.Data {
+		for _, class := range db.classes.Data {
+			for _, lesson := range db.lessons.Data {
+				for _, exam := range db.exams.Data {
+					if timetable.ClassID == class.ID && class.Year == year && class.Mod == mod && lesson.ID == exam.LessonID {
+						str = append(str, lesson.Name)
+					}
+				}
+			}
+		}
+	}
+	return unique(str)
+}
+
+func averageGradeForStudents(db tables, name string) (average float64) {
+	var gr []int
+	for _, exam := range db.exams.Data {
+		for _, student := range db.students.Data {
+			if name == student.Name && exam.StudentID == student.ID {
+				average += float64(exam.Grade)
+				gr = append(gr, exam.Grade)
+			}
+		}
+	}
+	return average / float64(len(gr))
+}
+
 func main() {
 	var db tables
+	year := 10
+	mod := "A"
+	n := "Alina"
 
 	db.groups.AddStudentsToClass(
 		db.classes.Insert(10, "B"),
@@ -66,6 +122,7 @@ func main() {
 	maths := db.lessons.Insert("Mathematics")
 	programming := db.lessons.Insert("Programming")
 	sports := db.lessons.Insert("Sports")
+	physics := db.lessons.Insert("Physics")
 
 	db.timetable.AddTimetable(
 		db.classes.MustFind(10, "A"),
@@ -84,9 +141,23 @@ func main() {
 		time.Tuesday,
 		maths,
 	)
+	db.timetable.AddTimetable(
+		db.classes.MustFind(10, "B"),
+		time.Tuesday,
+		physics,
+	)
+	db.exams.Insert(1, 9, 1)
+	db.exams.Insert(1, 8, 2)
+	db.exams.Insert(2, 8, 3)
+	db.exams.Insert(5, 8, 3)
+	db.exams.Insert(5, 7, 2)
+	db.exams.Insert(5, 9, 1)
 
 	fmt.Println("Student count per class", studentCountPerClass(db))
 	fmt.Println("Student count per year", studentCountPerYear(db))
+	fmt.Println("Lessons per year", lessonsPerYear(db, year))
+	fmt.Println("Exams per class", examsPerClass(db, year, mod))
+	fmt.Println("Average grade for student", averageGradeForStudents(db, n))
 
 	fmt.Println("We have", len(db.students.Data), "unique students in our school")
 	fmt.Println("We have", len(db.classes.Data), "unique classes in our school")
